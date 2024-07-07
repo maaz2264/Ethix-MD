b406fcc92a48043795b12fd78e880eeac384ce58
maaz2264 <143191256+maaz2264@users.noreply.github.com>
2024-07-07 13:11:19 +0500

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -18,7 +18,7 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || 'public',
-  OWNER_NAME: process.env.OWNER_NAME || "✪⏤͟͞★⃝ꪶ‎𝞢𝙏𝞖𝞘𝞦-𝞛𝘿𖥘✪͜͡➺",
+  OWNER_NAME: process.env.OWNER_NAME || "˜”°•✩•°”˜ 𝙼𝚊𝚊𝚣 ˜”°•✩•°”˜",
   OWNER_NUMBER: process.env.OWNER_NUMBER || "923192106398",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
